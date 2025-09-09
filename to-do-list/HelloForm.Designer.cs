1cfa9c83e2f9c08770ebcbcf6915105a0a23a800
Dina Ragab <dinaragab982005@gmail.com>
2025-09-09 21:49:47 +0300

    Add project files.

---
+++ to-do-list/HelloForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace to_do_list
+{
+    partial class HelloForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelloForm));
+            Hellolabel = new Label();
+            Tasktxt = new TextBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            checkedListBox1 = new CheckedListBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // Hellolabel
+            // 
+            Hellolabel.AutoSize = true;
+            Hellolabel.BackColor = Color.Transparent;
+            Hellolabel.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Hellolabel.ForeColor = SystemColors.ActiveCaptionText;
+            Hellolabel.Location = new Point(366, 32);
+            Hellolabel.Name = "Hellolabel";
+            Hellolabel.Size = new Size(303, 54);
+            Hellolabel.TabIndex = 0;
+            Hellolabel.Text = "My To-Do-List";
+            // 
+            // Tasktxt
+            // 
+            Tasktxt.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Tasktxt.Location = new Point(85, 204);
+            Tasktxt.Multiline = true;
+            Tasktxt.Name = "Tasktxt";
+            Tasktxt.Size = new Size(286, 50);
+            Tasktxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(112, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 45);
+            label1.TabIndex = 2;
+            label1.Text = "Enter a task";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Low", "Meduim", "High" });
+            comboBox1.Location = new Point(85, 299);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(286, 46);
+            comboBox1.TabIndex = 3;
+            comboBox1.Text = "Priorty";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(150, 402);
+            button1.Name = "button1";
+            button1.Size = new Size(102, 50);
+            button1.TabIndex = 4;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(655, 392);
+            button2.Name = "button2";
+            button2.Size = new Size(285, 60);
+            button2.TabIndex = 5;
+            button2.Text = "Clear All";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(717, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(156, 45);
+            label2.TabIndex = 6;
+            label2.Text = "My Tasks";
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(604, 168);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(418, 184);
+            checkedListBox1.TabIndex = 7;
+            checkedListBox1.ItemCheck += checkedListBox1_ItemCheck;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.istockphoto_2182495161_612x612_removebg_preview;
+            pictureBox1.Location = new Point(793, 60);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(229, 128);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(238, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(212, 109);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(-18, 32);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(1081, 476);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 10;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(855, 299);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(279, 245);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 11;
+            pictureBox4.TabStop = false;
+            // 
+            // HelloForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1098, 505);
+            Controls.Add(checkedListBox1);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(Tasktxt);
+            Controls.Add(Hellolabel);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Name = "HelloForm";
+            Text = "To-Do-List";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Hellolabel;
+        private TextBox Tasktxt;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+        private CheckedListBox checkedListBox1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+    }
+}
---
+++ to-do-list/HelloForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace to_do_list
+{
+    public partial class HelloForm : Form
+    {
+        public HelloForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Tasktxt.Text) && comboBox1.SelectedItem != null)
+            {
+                string task = Tasktxt.Text;
+                string priority = comboBox1.SelectedItem.ToString();
+                if (priority == "High")
+                {
+                    checkedListBox1.Items.Insert(0, "High: " + task);
+                }
+                else if (priority == "Meduim")
+                {
+                    int lastHigh = -1;
+                    for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                    {
+                        if (checkedListBox1.Items[i].ToString().StartsWith("High: "))
+                        {
+                            lastHigh = i;
+                        }
+                    }
+                    if (lastHigh == -1)
+                    {
+                        checkedListBox1.Items.Insert(0, "Meduim: " + task);
+                    }
+                    else
+                        checkedListBox1.Items.Insert(lastHigh + 1, "Meduim: " + task);
+                }
+                else
+                {
+                    checkedListBox1.Items.Add("Low: " + task);
+                }
+                Tasktxt.Clear();
+            }
+            else
+            {
+                MessageBox.Show("please enter task first and priority");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.Items.Count > 0)
+            {
+                checkedListBox1.Items.Clear();
+
+            }
+            else
+            {
+                MessageBox.Show("No tasks to delete");
+            }
+
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked)
+            {
+                MessageBox.Show("Gongratulations!!!!!🎉🎉");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
